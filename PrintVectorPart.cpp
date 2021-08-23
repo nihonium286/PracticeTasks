c78f01d54747592c89d13cd41b1b91c00474246e
Volodymyr Povoroznyk <35665053+nihonium286@users.noreply.github.com>
2021-08-23 03:25:55 +0300

    Add files via upload

---
+++ PrintVectorPart.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+void PrintVectorPart(const vector<int>& numbers) {
+	auto f_it = find(numbers.begin(),
+		numbers.end(),
+		[](const int& n) {
+		return n < 0;
+		});
+
+	auto it = f_it;
+
+	while (it != numbers.begin()) {
+		--it;
+		cout << *it;
+	}
+
+}
+
+int main() {
+	PrintVectorPart({ 6, 1, 8, -5, 4 });
+	cout << endl;
+	PrintVectorPart({ -6, 1, 8, -5, 4 });  // ничего не выведется
+	cout << endl;
+	PrintVectorPart({ 6, 1, 8, 5, 4 });
+	cout << endl;
+	return 0;
+}
