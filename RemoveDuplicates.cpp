d8c0f6f15401b7bf23d2ec92b0dff95152baf99b
Volodymyr Povoroznyk <35665053+nihonium286@users.noreply.github.com>
2021-08-26 03:53:12 +0300

    Add files via upload

---
+++ RemoveDuplicates.cpp
@@ -0,0 +1,38 @@
+/*
+Напишите шаблонную функцию RemoveDuplicates, принимающую по ссылке вектор elements объектов типа T 
+и удаляющую из него все дубликаты элементов. Порядок оставшихся элементов может быть любым.
+
+Гарантируется, что объекты типа T можно сравнивать с помощью операторов ==, !=, < и >.
+*/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+void RemoveDuplicates(vector<T>& elements) {
+	set<T> n(elements.begin(), elements.end());
+	elements.erase(elements.begin(), elements.end());
+	elements.insert(elements.begin(), n.begin(), n.end());
+}
+
+int main() {
+	vector<int> v1 = { 6, 4, 7, 6, 4, 4, 0, 1 };
+	RemoveDuplicates(v1);
+	for (int x : v1) {
+		cout << x << " ";
+	}
+	cout << endl;
+	
+    // operator << overload for string is neccessary for this part to work
+	/* 
+	vector<string> v2 = { "C", "C++", "C++", "C", "C++" };
+	RemoveDuplicates(v2);
+	for (const string& s : v2) {
+		cout << s << " ";
+	}
+	cout << endl; */
+	return 0;
+}
