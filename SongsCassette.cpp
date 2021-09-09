81b7444dcb2f8b0aa3091bf44371c70781e5bad2
Volodymyr Povoroznyk <35665053+nihonium286@users.noreply.github.com>
2021-09-09 14:00:45 +0300

    Add files via upload

--- SongsCassette.cpp
+++ SongsCassette.cpp
@@ -1,30 +1,54 @@
+#include "pch.h"
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 
+template<class BidirIt>
+void quick(BidirIt first, BidirIt last, int m, int d) {
+
+	auto pivot = *std::next(first, rand() % std::distance(first, last));
+
+	BidirIt part = std::partition(first, last, [pivot](const int & x) {return x <= pivot; });
+
+	int dist = std::distance(first, part) + d;
+
+	if (dist == m) {
+		return;
+	}
+	else if (dist < m) {
+		quick(part, last, m, dist);
+	}
+	else if (dist > m) {
+		if (std::equal(first + 1, part, first))
+		{
+			return;
+		}
+		quick(first, part, m, 0);
+	}
+
+}
 
 int main()
 {
-
 	size_t n, m;
 	int S, A, B, M;
 
 	std::cin >> n >> m >> S >> A >> B >> M;
 
-	std::multiset<int> X{ S };
-
-	int X_prev{ S };
-
-	for (size_t i{}; i < n - 1; i++) {
-		X.insert((A * X_prev + B) % M);
-		X_prev = (A * X_prev + B) % M;
+	std::vector<int> v{S};
+	
+	for (size_t i{ 1 }; i < n; i++) {
+		v.push_back((A * v[i-1] + B) % M);
 	}
+
+	quick(v.begin(), v.end(), m, 0);
+
+	std::vector<int> n_v(v.begin(), v.end());
 
 	int sum{};
 
-	size_t i{};
-
-	for (std::multiset<int>::iterator it{ X.begin() }; i < m; it++, i++) {
-		sum += *it;
+	for (size_t i{}; i < m; i++) {
+		sum += n_v[i];
 	}
 
 	std::cout << sum;
